f83b8ce81d47c5342e1058c9e768420510eefb34
apoortinga <poortinga.ate@gmail.com>
2017-09-26 17:44:11 +0700

    export function

--- model.py
+++ model.py
@@ -12,12 +12,11 @@ class environment(object):
     
     
     def __init__(self):
-          
-        # set the logfile
-              
+        """Initialize the environment."""   
+         
         # Initialize the Earth Engine object, using the authentication credentials.
         ee.Initialize()
-    
+       
         # set dates
         self.startYear = 1999;
         self.endYear = 2008;
@@ -36,12 +35,22 @@ class environment(object):
 
         # whether to use imagecolletions
         self.useL4=True
-        self.useL5=False
+        self.useL5=True
         self.useL7=True
-        self.useL8=False
+        self.useL8=True
 
-        self.maskSR = False;
-        self.maskCF = False;
+        # apply cloud masks
+        self.maskSR = True
+        self.maskCF = True
+        
+        # apply defringe
+        self.defringe = True
+        
+        # pixel size
+        self.pixSize = 100
+        
+        # user ID
+        self.userID = "users/servirmekong/temp/"
         
         # define the landsat bands
         self.sensorBandDictLandsatSR = ee.Dictionary({'L8' : ee.List([1,2,3,4,5,6,7]),
@@ -52,10 +61,58 @@ class environment(object):
         # threshold for defringing landsat5 and 7
         self.fringeCountThreshold = 279;                              
 
-
-class landCoverTool():
+        self.k = ee.Kernel.fixed(41, 41, 
+                                [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+        
+        
+        
+class SurfaceReflectance():
     
     def __init__(self):
+        """Initialize the Surfrace Reflectance app."""  
+        
+        # import the log library
+        
         import logging
         # set the logfile
         logging.basicConfig(filename='model.log', filemode='w', level=logging.DEBUG)
@@ -66,90 +123,111 @@ class landCoverTool():
         # get the environment
         self.env = environment()
     
-    # run the model
-    def runModel(self):
+    def RunModel(self):
+        """Run the SR model"""  
         
-        logging.info('running the model')
+        logging.info('starting the model the model')
         
-        collection = self.getLandsat()
+        # get the images
+        collection = self.GetLandsat()
         
-        percentiles = self.calculatePercentiles(collection)
+        img = ee.Image(collection.first())
+        self.ExportToAsset(img,"test")
         
-        #filteredCollection = self.filterTime(collection) 
-        
-        filteredCollection = self.filterBounds(collection) 
-            
-                
+        # calculate the percentiles
+        #percentiles = self.CalculatePercentiles(collection)
+                   
         #print filteredCollection
-        return  filteredCollection
+        return  collection
         
           
     # Obtain Landsat image collections 
-    def getLandsat(self):
+    def GetLandsat(self):
+        """Get the Landsat imagery"""  
         
         logging.info('getting landsat images')
-        
-        collection = []
-		
+
+        # construct date objects
         startDate = ee.Date.fromYMD(self.env.startYear,1,1)
-        endDate = ee.Date.fromYMD(self.env.endYear,1,1)
+        endDate = ee.Date.fromYMD(self.env.endYear,1,1)        
         
-  
+        # boolean to merge Landsat; when true is merges with another collection
         merge = False
-        # landsat image collections 
+        
+        # landsat4 image collections 
         if self.env.useL4:        
             landsat4 =  ee.ImageCollection('LANDSAT/LT4_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
+            landsat4 = landsat4.filterMetadata('CLOUD_COVER','less_than',self.env.metadataCloudCoverMax)
+            if self.env.defringe == True:
+                 landsat4 =  landsat4.map(self.DefringeLandsat)
             if self.env.maskSR == True:
-                landsat4 = landsat4.map(self.cloudMaskSR)
+                landsat4 = landsat4.map(self.CloudMaskSR)
             if self.env.maskSR == True:
-                landsat4 = landsat4.map(self.cloudMaskCF)
+                landsat4 = landsat4.map(self.CloudMaskCF)
             if not merge:
-				landsatCollection = landsat4
-				merge = True
+			landsatCollection = landsat4
+			merge = True
 
+        # landsat 5 image collections 
         if self.env.useL5:
             landsat5 =  ee.ImageCollection('LANDSAT/LT5_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
+            landsat5 = landsat5.filterMetadata('CLOUD_COVER','less_than',self.env.metadataCloudCoverMax)
+            if self.env.defringe == True:
+                 landsat5 =  landsat5.map(self.DefringeLandsat)
             if self.env.maskSR == True:
-                landsat5 = landsat5.map(self.cloudMaskSR)
+                landsat5 = landsat5.map(self.CloudMaskSR)
             if self.env.maskSR == True:
-                landsat5 = landsat5.map(self.cloudMaskCF)
+                landsat5 = landsat5.map(self.CloudMaskCF)
             if not merge:
-				landsatCollection = landsat5
-				merge = True
+			landsatCollection = landsat5
+			merge = True
             else:
-				landsatCollection = landsatCollection.merge(landsat5)
+			landsatCollection = landsatCollection.merge(landsat5)
         
+        # landsat 7 image collections  
         if self.env.useL7:
             landsat7 =  ee.ImageCollection('LANDSAT/LE7_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
+            landsat7 = landsat7.filterMetadata('CLOUD_COVER','less_than',self.env.metadataCloudCoverMax)
+            if self.env.defringe == True:
+                 landsat7 =  landsat7.map(self.DefringeLandsat)            
             if self.env.maskSR == True:
-                landsat7 = landsat7.map(self.cloudMaskSR)
+                landsat7 = landsat7.map(self.CloudMaskSR)
             if self.env.maskSR == True:
-                landsat7 = landsat7.map(self.cloudMaskCF)
+                landsat7 = landsat7.map(self.CloudMaskCF)
             if not merge:
-				landsatCollection = landsat7
-				merge = True
+			landsatCollection = landsat7
+			merge = True
             else:
-				landsatCollection = landsatCollection.merge(landsat7)
-				        
+			landsatCollection = landsatCollection.merge(landsat7)
+
+        # landsat8  image collections 				        
         if self.env.useL8:
             landsat8 =  ee.ImageCollection('LANDSAT/LC8_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
+            landsat8 = landsat8.filterMetadata('CLOUD_COVER','less_than',self.env.metadataCloudCoverMax)
+            if self.env.defringe == True:
+                 landsat8 =  landsat8.map(self.DefringeLandsat)    
             if self.env.maskSR == True:
-                landsat8 = landsat8.map(self.cloudMaskCF)            
+                landsat8 = landsat8.map(self.CloudMaskCF)            
             if not merge:
-				landsatCollection = landsat8
-				merge = True
+			landsatCollection = landsat8
+			merge = True
             else:
-				landsatCollection = landsatCollection.merge(landsat8)            
-          
+			landsatCollection = landsatCollection.merge(landsat8)            
+       
+        count = landsatCollection.size();
+        
+        logging.info('scaling images');
+        landsatCollection = landsatCollection.map(self.ScaleLandsat)
+       
+        logging.info('counted ' + str(count.getInfo()) +' images');           
         logging.info('returning imagecollection')
         
-        
-        #logging.info(landsat8.getInfo())
-            
+        # return the image collection           
         return ee.ImageCollection(landsatCollection)
         
 
-    def calculatePercentiles(self,collection):
+    def CalculatePercentiles(self,collection):
+        """Calculate percentiles to filter imagery"""  
         
         logging.info('calculate percentiles')
         
@@ -157,65 +235,73 @@ class landCoverTool():
         collectionPercentile = collection.reduce(ee.Reducer.percentile([self.env.lowPercentile,self.env.highPercentile])) 
         
         logging.info('returning percentiles')
+        
         return collectionPercentile
-        
-        
-    # apply filter for period
-    def filterTime(self,collection):
-        
-        logging.info('Filter for time')
-        
-        # construct date objects
-        startDate = ee.Date.fromYMD(self.env.startYear,1,1)
-        endDate = ee.Date.fromYMD(self.env.endYear,1,1)
-        
-        # filter for year
-        filteredCollection = collection.filterDate(startDate,endDate) #.filter(ee.Filter.calendarRange(self.env.startJulian,self.env.endJulian))
-        
-        logging.info('return filtered collection')
-        return  filteredCollection
-        
-    # apply filter for area
-    def filterBounds(self,collection):
-        
-        logging.info('Filter for location')
-                       
-        # filter for year
-        filteredCollection = collection.filterBounds(self.env.location)
-        
-        logging.info('return filtered collection')
-        return  filteredCollection
-        
-    def cloudMaskSR(self,img):
-        
-        logging.info('Applying SR mask')
-        
-        # get cloud mask
-        fmk = img.select("sr_cloud_qa").mask();
-        
-        logging.info('return mask')
-        return img.updateMask(fmk) ## add .not
-        
-    def cloudMaskCF(self,img):
-        fmk = img.select("cfmask").lt(2)
-        return img.updateMask(fmk)
 
         
+    def CloudMaskSR(self,img):
+         """Apply cloud mask"""  
+       
+         logging.info('Applying SR mask')
+        
+         fmk = img.select("sr_cloud_qa").mask();
+        
+         logging.info('return mask')
+         return img.updateMask(fmk) ## add .not
+        
+    def CloudMaskCF(self,img):
+         """apply cf-mask Landsat"""  
+        
+         fmk = img.select("cfmask").lt(2)
+         return img.updateMask(fmk)
+
+    def ScaleLandsat(self,img):
+        """Landast is scaled by factor 0.0001 """
+        
+        scaled = ee.Image(img).multiply(0.0001).copyProperties(img,['system:time_start']) 
+        logging.info('return scaled image')
+        return scaled
+
+    def  DefringeLandsat(self,img):   
+        """remove scanlines from landsat 4 and 7 """
+        
+        logging.info('removing scanlines')
+
+        m = ee.Image(img).select("B1").mask().reduce(ee.Reducer.min())
+        sum = m.reduceNeighborhood(ee.Reducer.sum(), self.env.k, 'kernel')
+        sum = sum.gte(self.env.fringeCountThreshold)        
+        img = img.mask(img.mask().add(sum)) # Check this
+        return img
+        
+    def ExportToAsset(self,img,assetName):  
+        """export to asset """
+        
+        outputName = self.env.userID + assetName
+        logging.info('export image to asset: ' + str(outputName))       
+        
+        task_ordered = ee.batch.Export.image.toAsset(ee.Image(img), "exportJob", outputName, maxPixels=1e13)
+        
+        # start task
+        task_ordered.start()
+    
         
         
-col = landCoverTool().runModel()
+col = SurfaceReflectance().RunModel()
    
 count = col.size();
 print('Count: ', count.getInfo());
 
-image = ee.Image(col.median())
+image = ee.Image(col.first())
+
+
 
 #print(image)
 # create the vizualization parameters
 viz = {'min':0.0, 'max':2000, 'bands':["B3","B2","B1"]};
 # Print the information for an image asset.
 
-#from PIL import Image, ImageTk
-import ee.mapclient
+
+#import ee.mapclient
+
 # display the map
 #ee.mapclient.addToMap(ee.Image(image),viz)
