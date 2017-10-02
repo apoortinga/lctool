2dc3dd28e0461e3a71b0844d58252f85935870e8
Ate <poortinga.ate@gmail.com>
2017-10-02 09:29:10 +0700

    updated cloud fiter

--- model.py
+++ model.py
@@ -19,17 +19,27 @@ class environment(object):
         ee.Initialize()
        
 	self.timeString = time.strftime("%Y%m%d_%H%M%S")
+
+	# SEASONS:
+	# '0': Dry Cool: Nov - Feb (305 - 59)
+	# '1': Dry Hot: Mar - Apr (60 - 120)
+	# '2': Rainy: May - Oct (121 - 304)
        
         # set dates
-        self.startYear = 2002;
+        self.startYear = 2003;
         self.endYear = 2003;
-        self.startJulian = 1;
-        self.endJulian = 366;        
+        self.startJulian = 305;
+        self.endJulian = 59;        
         
+	
+	# Rainy
+	# dry hot
+	# dry cool
+	
         # set location 
-        #self.location = ee.Geometry.Polygon([[[103.294,17.923],[103.294,17.923],[106.453,17.923],[106.453,20.469],[103.2941,20.469],[103.294,17.923]]])
+        self.location = ee.Geometry.Polygon([[[103.294,17.923],[103.294,17.923],[106.453,17.923],[106.453,20.469],[103.2941,20.469],[103.294,17.923]]])
         
-        self.location = ee.Geometry.Polygon([[[103.91,20.02],[103.910,20.601],[103.1,20.601],[103.1325,20.002],[104.919,20.020]]]) #ee.Geometry.Point([105.809,21.074])
+        #self.location = ee.Geometry.Polygon([[[103.91,20.02],[103.910,20.601],[103.1,20.601],[103.1325,20.002],[104.919,20.020]]]) #ee.Geometry.Point([105.809,21.074])
         
         # variable to filter cloud threshold
         self.metadataCloudCoverMax = 25
@@ -140,7 +150,7 @@ class SurfaceReflectance():
         
         # construct date objects
         startDate = ee.Date.fromYMD(self.env.startYear,1,1)
-        endDate = ee.Date.fromYMD(self.env.endYear,1,1)    
+        endDate = ee.Date.fromYMD(self.env.endYear,12,31)    
         
 	logging.info('startDate = ' + str(startDate.getInfo()))
 	logging.info('endDatDate = ' + str(startDate.getInfo()))
@@ -154,10 +164,11 @@ class SurfaceReflectance():
         # calculate the percentiles
         
         #print percentiles.getInfo()
-	
-        collection = collection.map(self.MaskPercentile) 
+	self.percentile = ee.Image(self.CalculatePercentiles())  
+        
+	collection = collection.map(self.MaskPercentile) 
 
-	img = ee.Image(collection.sum())  
+	img = ee.Image(collection.median())  
 
 	self.ExportToAsset(img,outputName)         
        
@@ -178,6 +189,7 @@ class SurfaceReflectance():
         if self.env.useL4:        
             landsat4 =  ee.ImageCollection('LANDSAT/LT4_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
             landsat4 = landsat4.filterMetadata('CLOUD_COVER','less_than',metadataCloudCoverMax)
+            landsat4 = landsat4.filter(ee.Filter.calendarRange(self.env.startJulian,self.env.endJulian))
             if self.env.defringe == True:
                  landsat4 =  landsat4.map(self.DefringeLandsat)
             if self.env.maskSR == True:
@@ -192,6 +204,7 @@ class SurfaceReflectance():
         if self.env.useL5:
             landsat5 =  ee.ImageCollection('LANDSAT/LT5_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
             landsat5 = landsat5.filterMetadata('CLOUD_COVER','less_than',metadataCloudCoverMax)
+            landsat5 = landsat5.filter(ee.Filter.calendarRange(self.env.startJulian,self.env.endJulian))
             if self.env.defringe == True:
                  landsat5 =  landsat5.map(self.DefringeLandsat)
             if self.env.maskSR == True:
@@ -208,6 +221,7 @@ class SurfaceReflectance():
         if self.env.useL7:
             landsat7 =  ee.ImageCollection('LANDSAT/LE7_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
             landsat7 = landsat7.filterMetadata('CLOUD_COVER','less_than',metadataCloudCoverMax)
+            landsat7 = landsat7.filter(ee.Filter.calendarRange(self.env.startJulian,self.env.endJulian))
             if self.env.defringe == True:
                  landsat7 =  landsat7.map(self.DefringeLandsat)            
             if self.env.maskSR == True:
@@ -224,6 +238,7 @@ class SurfaceReflectance():
         if self.env.useL8:
             landsat8 =  ee.ImageCollection('LANDSAT/LC8_SR').filterDate(startDate,endDate).filterBounds(self.env.location)
             landsat8 = landsat8.filterMetadata('CLOUD_COVER','less_than',metadataCloudCoverMax)
+            landsat8 = landsat8.filter(ee.Filter.calendarRange(self.env.startJulian,self.env.endJulian))
             if self.env.defringe == True:
                  landsat8 =  landsat8.map(self.DefringeLandsat)    
             if self.env.maskSR == True:
@@ -313,22 +328,21 @@ class SurfaceReflectance():
 	lower = str(int(self.env.lowPercentile))
 	
 	# we dont want to include the mask
-	selectedBandNamesLandsat = ee.List(['blue','green','red','nir','swir1','swir2'])
+	selectedBandNamesLandsat = ee.List(['blue'])#,'green','red','nir','swir1','swir2'])
 
 	# get the upper and lower band
-	bandsUpper = ee.List(['blue_p' + upper,'green_p'+ upper,'red_p'+ upper,'nir_p'+ upper,'swir1_p'+ upper,'swir2_p'+ upper])
-	bandsLower = ee.List(['blue_p' + lower,'green_p'+ lower,'red_p'+ lower,'nir_p'+ lower,'swir1_p'+ lower,'swir2_p'+ lower])
-	
-	percentile = ee.Image(self.CalculatePercentiles())  
-	percentilesUp = percentile.select(bandsUpper,selectedBandNamesLandsat )
-	percentilesLow = percentile.select(bandsLower,selectedBandNamesLandsat )
+	bandsUpper = ee.List(['blue_p'+ upper ])#+ upper,'green_p'+ upper,'red_p'+ upper,'nir_p'+ upper,'swir1_p'+ upper,'swir2_p'+ upper])
+	bandsLower = ee.List(['blue_p' + lower])#+ lower,'green_p'+ lower,'red_p'+ lower,'nir_p'+ lower,'swir1_p'+ lower,'swir2_p'+ lower])
+
+	percentilesUp = self.percentile.select(bandsUpper,selectedBandNamesLandsat )
+	percentilesLow = self.percentile.select(bandsLower,selectedBandNamesLandsat )
 	
 	imgToMask = img.select(selectedBandNamesLandsat)
 			 
-	#darkMask = ee.Image(imgToMask.lt(percentilesLow).reduce(ee.Reducer.sum())).eq(0)
+	darkMask = ee.Image(imgToMask.lt(percentilesLow).reduce(ee.Reducer.sum())).eq(0)
 	lightMask = ee.Image(imgToMask.gt(percentilesUp).reduce(ee.Reducer.sum())).eq(0)
 	    
-	return img.updateMask(lightMask) #img.updateMask(darkMask).updateMask(lightMask)
+	return img.updateMask(lightMask).updateMask(darkMask)
 
  
     def landsatCloudScore(self,img):
@@ -342,16 +356,21 @@ class SurfaceReflectance():
       
 	# Compute several indicators of cloudiness and take the minimum of them.
         # Clouds are reasonably bright in the blue band.
-
+	
 	thresholds  = [0.075, 0.3]
 	score = ee.Image(img.select("blue").subtract(thresholds[0])).divide(thresholds[1] - thresholds[0])
  
+	thresholds  = [0.2, 0.8]
+	image = img.select("blue").add(img.select("red")).add(img.select("green"))
         # Clouds are reasonably bright in all visible bands.
-        #score = score.min(self.rescale(image, 'red', [0.2, 0.8]));
+        score = score.min(ee.Image(image.subtract(thresholds[0])).divide(thresholds[1] - thresholds[0]))
+	#self.rescale(image, 'red', [0.2, 0.8]));
    
-        #image = ee.Image(img.select('nir').add(img.select('swir1')).add(img.select('swir2')))
+	thresholds  = [0.2, 0.8]
+        image = ee.Image(img.select('nir').add(img.select('swir1')).add(img.select('swir2')))
         # Clouds are reasonably bright in all infrared bands.
-        #score = score.min(self.rescale(image, 'nir', [0.3, 0.8]));
+        score = score.min(ee.Image(image.subtract(thresholds[0])).divide(thresholds[1] - thresholds[0]))
+	
 
         # edit Ate cfmask uses therman band to detect clouds
         # Clouds are reasonably cool in temperature.
@@ -371,6 +390,8 @@ class SurfaceReflectance():
         
         outputName = self.env.userID + str(self.env.timeString) + assetName
         logging.info('export image to asset: ' + str(outputName))   
+	
+	img = img.multiply(10000).int16()
 
                     
         task_ordered = ee.batch.Export.image.toAsset(image=ee.Image(img), description=str(self.env.timeString)+"_exportJob", assetId=outputName,region=self.env.location['coordinates'], maxPixels=1e13,scale=self.env.pixSize)
